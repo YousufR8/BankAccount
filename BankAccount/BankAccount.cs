c829886358aa4cd079c3a325219c1e89a7cb22fc
YousufR8 <Yrehman06@gmail.com>
2023-09-27 11:04:42 -0400

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in _allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private static int s_accountNumberSeed = 12345678;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Owner = name;
+
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+        }
+
+        private List<Transaction> _allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            _allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException(nameof(amount));
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("not sufficient funds for this withdrawl");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            _allTransactions.Add(withdrawl);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var transaction in _allTransactions)
+            {
+                balance += transaction.Amount;
+                report.AppendLine($"{transaction.Date.ToString()}\t{transaction.Amount}\t{transaction.Notes}");
+            }
+
+            return report.ToString();
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Jon", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawl(500, DateTime.Now, "Rent");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(500, DateTime.Now, "Paycheck");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                account.MakeWithdrawl(2000, DateTime.Now, "overDrawl");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("overdrawl fees will be applied");
+                Console.WriteLine(ex.ToString());
+            }
+
+            try
+            {
+                var invalidAccount = new BankAccount("jeff", -100);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception trying to create account with negitive ballence");
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            Console.ReadLine();
+        }
+    }
+}
